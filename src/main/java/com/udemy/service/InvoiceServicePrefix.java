640b519debee3bb9479156edcce257684533fa85
villiseck <villiseck@abes.fr>
2023-09-25 16:47:33 +0200

    @Value mise en place

--- src/main/java/com/udemy/service/InvoiceServicePrefix.java
+++ src/main/java/com/udemy/service/InvoiceServicePrefix.java
@@ -3,16 +3,15 @@ package com.udemy.service;
 import com.udemy.entity.Invoice;
 import com.udemy.repository.InvoiceRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 public class InvoiceServicePrefix implements InvoiceServiceInterface {
+    @Value("124")
     private long lastNumber;
+    @Value("NUM_")
     private String prefix;
     @Autowired
     private InvoiceRepositoryInterface invoiceRepositoryMichel;
-
-    public InvoiceRepositoryInterface getInvoiceRepositoryMichel() {
-        return invoiceRepositoryMichel;
-    }
 
     public void setInvoiceRepository(InvoiceRepositoryInterface invoiceRepositoryMichel) {
         this.invoiceRepositoryMichel = invoiceRepositoryMichel;
@@ -21,20 +20,5 @@ public class InvoiceServicePrefix implements InvoiceServiceInterface {
     public void createInvoice(Invoice invoice){
         invoice.setNumber(prefix+(++lastNumber));
         invoiceRepositoryMichel.create(invoice);
-    }
-    public long getLastNumber() {
-        return lastNumber;
-    }
-
-    public void setLastNumber(long lastNumber) {
-        this.lastNumber = lastNumber;
-    }
-
-    public String getPrefix() {
-        return prefix;
-    }
-
-    public void setPrefix(String prefix) {
-        this.prefix = prefix;
     }
 }

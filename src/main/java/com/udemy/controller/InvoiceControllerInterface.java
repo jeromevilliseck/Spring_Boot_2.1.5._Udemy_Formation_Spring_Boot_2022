74c115cc2bdffec914af4a383b0d755aaa0fde93
villiseck <villiseck@abes.fr>
2023-09-25 16:36:57 +0200

    Autowiring sur bean service, suppression du setter

--- src/main/java/com/udemy/controller/InvoiceControllerInterface.java
+++ src/main/java/com/udemy/controller/InvoiceControllerInterface.java
@@ -1,9 +1,6 @@
 package com.udemy.controller;
 
-import com.udemy.service.InvoiceServiceInterface;
-
 public interface InvoiceControllerInterface {
     void createInvoice();
 
-    void setInvoiceService(InvoiceServiceInterface invoiceService);
 }
--- src/main/java/com/udemy/controller/InvoiceControllerWeb.java
+++ src/main/java/com/udemy/controller/InvoiceControllerWeb.java
@@ -7,18 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class InvoiceControllerWeb implements InvoiceControllerInterface {
     @Autowired
     private InvoiceServiceInterface service;
+    //L'annotation @Autowired permet de s'affranchir du setter
+    //La visibilité est transgressée par l'onnotation qui intervient au niveau du bytecode
+    //L'autowired peut etre placé sur le setter d'un service pour forcer spring a passer par le setter lors de la DI
+
     public void createInvoice() {
         String customerName = "Tesla";
         Invoice invoice = new Invoice();
         invoice.setCustomerName(customerName);
         service.createInvoice(invoice);
-    }
-
-    public InvoiceServiceInterface getService() {
-        return service;
-    }
-
-    public void setInvoiceService(InvoiceServiceInterface service) {
-        this.service = service;
     }
 }

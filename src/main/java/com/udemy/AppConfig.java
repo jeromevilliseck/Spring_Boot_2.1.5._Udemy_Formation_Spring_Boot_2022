ca146ea0fbef29ecfebd97f386c0735c1771eeba
villiseck <villiseck@abes.fr>
2023-09-26 11:55:09 +0200

    Changement de lancement de l'APP par classe dédiée à la configuration

--- src/main/java/com/udemy/AppConfig.java
+++ src/main/java/com/udemy/AppConfig.java
@@ -2,10 +2,12 @@ package com.udemy;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = {"com.udemy.controller.scan", "com.udemy.service.number", "com.udemy.repository.database"})
 @PropertySource("classpath:application.properties")
+@ImportResource("")
 public class AppConfig {
 }

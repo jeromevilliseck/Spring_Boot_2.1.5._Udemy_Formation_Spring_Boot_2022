dec674e3fe748b06ece9518dcaebde3bf1a76a47
villiseck <villiseck@abes.fr>
2023-09-26 14:43:42 +0200

    Placement de la configuration dans la classe App principale

--- src/main/java/com/udemy/App.java
+++ src/main/java/com/udemy/App.java
@@ -3,15 +3,17 @@ package com.udemy;
 import com.udemy.controller.InvoiceControllerInterface;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-/**
- * Hello world!
- */
+@Configuration
+@ComponentScan(basePackages = {"com.udemy.controller.scan", "com.udemy.service.number", "com.udemy.repository.database"})
+@PropertySource("classpath:application.properties")
 public class App {
     public static void main(String[] args) {
         //On indique ou se situe la configuration pour l'IoC et la DI
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         InvoiceControllerInterface invoiceControllerInterface = context.getBean(InvoiceControllerInterface.class);
         invoiceControllerInterface.createInvoice();
     }

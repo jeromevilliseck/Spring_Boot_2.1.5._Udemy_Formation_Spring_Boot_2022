c7bb7e9569cd0a4fc053353facfc1b4c1b2896a0
villiseck <villiseck@abes.fr>
2023-09-26 16:15:47 +0200

    @SpringBootApplication avec entête SpringBoot

--- src/main/java/com/udemy/App.java
+++ src/main/java/com/udemy/App.java
@@ -2,6 +2,7 @@ package com.udemy;
 
 import com.udemy.controller.InvoiceControllerInterface;
 import com.udemy.service.prefix.InvoiceServicePrefix;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.*;
 @PropertySource("classpath:application.properties")
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
         InvoiceControllerInterface invoiceControllerInterface = context.getBean(InvoiceControllerInterface.class);
         invoiceControllerInterface.createInvoice();
     }

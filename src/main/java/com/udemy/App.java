8aae5c8d3a1f6a6cb2cca3aa4961f905f220e67f
villiseck <villiseck@abes.fr>
2023-09-28 13:51:10 +0200

    Conservation des dépendances minimales - Préparation à l'intégration continue avec Heroku

--- src/main/java/com/udemy/App.java
+++ src/main/java/com/udemy/App.java
@@ -7,9 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 
-//Ne fonctionne pas en l'état avec les versions > 2 de spring-boot-autoconfigure (pom.xml) ,
+//spring-boot-starter-parent encapsule désormais spring-boot-autoconfigure, spring-context (pom.xml)
 @SpringBootApplication
-@PropertySource("classpath:application.properties")
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(App.class);

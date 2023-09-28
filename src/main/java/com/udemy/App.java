9623907d29464329c5ceadc40163839208bd5234
villiseck <villiseck@abes.fr>
2023-09-28 15:06:52 +0200

    Test déploiement automatique

--- src/main/java/com/udemy/App.java
+++ src/main/java/com/udemy/App.java
@@ -1,11 +1,9 @@
 package com.udemy;
 
 import com.udemy.controller.InvoiceControllerInterface;
-import com.udemy.service.prefix.InvoiceServicePrefix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
 
 //spring-boot-starter-parent encapsule désormais spring-boot-autoconfigure, spring-context (pom.xml)
 @SpringBootApplication

a8b7869edd3f1bafd1f4e19f68f75a3a6b788719
villiseck <villiseck@abes.fr>
2023-09-29 09:15:04 +0200

    Application context explication

--- src/main/java/com/udemy/App.java
+++ src/main/java/com/udemy/App.java
@@ -5,11 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-//spring-boot-starter-parent encapsule désormais spring-boot-autoconfigure, spring-context (pom.xml)
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(App.class);
+        //L'application context sert pour faire des programmes console ou shell qui s'arrêtent après une exécution
+        ApplicationContext context = SpringApplication.run(App.class, args);
         InvoiceControllerInterface invoiceControllerInterface = context.getBean(InvoiceControllerInterface.class);
         invoiceControllerInterface.createInvoice();
     }

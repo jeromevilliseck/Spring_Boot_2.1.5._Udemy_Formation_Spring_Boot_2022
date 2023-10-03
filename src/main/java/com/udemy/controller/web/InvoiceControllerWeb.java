386819b1e2788add542cd37491f792c11309c6b9
villiseck <villiseck@abes.fr>
2023-10-03 14:12:01 +0200

    @Valid - ajout de la gestion des erreurs

--- src/main/java/com/udemy/controller/web/InvoiceControllerWeb.java
+++ src/main/java/com/udemy/controller/web/InvoiceControllerWeb.java
@@ -2,7 +2,9 @@ package com.udemy.controller.web;
 
 import com.udemy.controller.InvoiceControllerInterface;
 import com.udemy.entity.Invoice;
+import com.udemy.form.InvoiceForm;
 import com.udemy.service.InvoiceServiceInterface;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 //On va avoir des url pour un controleur Invoice qui commencent systématiquement par /invoice
 @Controller
 @RequestMapping("/invoice")
-public class InvoiceControllerWeb implements InvoiceControllerInterface {
+public class InvoiceControllerWeb {
     @Autowired
     private final InvoiceServiceInterface service;
 
@@ -20,16 +22,17 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
         this.service = service;
     }
 
-    //Avec @ModelAttribute Spring va instancier un objet invoice et le fournir en entrée
+    //Ajout de règles de validation
     @PostMapping( "")
-    public String createInvoice(@ModelAttribute Invoice invoice) {
+    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm) {
+        //Conversion InvoiceForm vers Invoice pour les besoins du service
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(invoiceForm.getCustomerName());
+        invoice.setOrderNumber(invoiceForm.getOrderNumber());
         service.createInvoice(invoice);
         return "invoice-created";
     }
 
-    //Spring utilise le request mapping pour trouver l'identifiant de la vue ici invoice-home
-    //spring cherche une page invoice-home dans resources -> templates
-    //L'annotation @ModelAttribute retourne un objet "invoices" aux vues
     @GetMapping("/home")
     public String displayHome(Model model){
         System.out.println("La méthod displayHome a été invoquée");
@@ -37,7 +40,6 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
         return "invoice-home";
     }
 
-    //{id} <-mapping-> @PathVariable("id") String number
     @GetMapping("/{id}")
     public String displayInvoice(@PathVariable("id") String number, Model model){
         System.out.println("La méthod displayInvoice a été invoquée");
@@ -46,7 +48,7 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
     }
 
     @GetMapping("/create-form")
-    public String displayInvoiceCreateForm(@ModelAttribute Invoice invoice){
+    public String displayInvoiceCreateForm(@ModelAttribute InvoiceForm invoice){
         return "invoice-create-form";
     }
 }
